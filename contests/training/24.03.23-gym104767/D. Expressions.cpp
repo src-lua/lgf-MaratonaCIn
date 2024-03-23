f4376fa3e2b2ec8c5639d229f06a992bea692eb1
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2024-03-23 12:05:25 -0300

    D - AC

--- contests/training/24.03.23-gym104767/D. Expressions.cpp
+++ contests/training/24.03.23-gym104767/D. Expressions.cpp
@@ -11,56 +11,70 @@ int main(){
 
     int n, q; cin >> n >> q;
 
-    vector<bool> group;
-    vector<pii> group_index;
-    group.push_back(true);
-    group_index.push_back({1,-1});
-    
+    vector<int> numbers(n);
+    vector<int> group;
+    // vector<pii> group_index;
+    vector<int> group_index;
+    group.push_back(0);
+    // group_index.push_back({1,-1});
+    group_index.push_back(0);
+
     int index = 0;
 
-    int x; char c;
+    char c;
     for (int i = 0; i < n-1; i++) {
-        cin >> x;
+        cin >> numbers[i];
 
-        if (x %2 == 1) group[index] = !group[index];
+        if (numbers[i] %2 == 0) group[index]++;
 
         cin >> c;
 
         if (c != '*') {
-            group_index[index] = {group_index.back().first, i+1};
-            group_index.push_back({i+2, -1});
-            group.push_back(true);
+            // group_index[index] = {group_index.back().first, i+1};
+            // group_index.push_back({i+2, -1});
+            group_index.push_back(i+1);
+            group.push_back(0);
             index++;
         }
     }
-    cin >> x;
-    if (x %2 == 1) group[index] = !group[index];
+    cin >> numbers[n-1];
+    if (numbers[n-1] %2 == 0) group[index]++;
+    group_index.push_back(n);
 
     int odd = 0;
 
     for(auto e: group) {
-        if (!e) odd++;
+        if (e == 0) odd++;
     }
 
-    if (odd%2==0) cout << "even";
-    else cout << "odd";
+    if (odd%2==0) cout << "even" << endl;
+    else cout << "odd" << endl;
 
-    int y;
-    for (int i = 0; i < q; i++) {
+    int x, y;
+    for (int j = 0; j < q; j++) {
         cin >> x >> y;
-
-        for (int i = 0; i < group_index.size(); i++) {
-            if (group_index[i].first < x && x < group_index[i].second) {
-                if ((group[i] && y%2!=0) || (!group[i] && y%2==0)) {
-                    group[i] = !group[i];
-                }
-                
-                break;
-            }
+        if (numbers[x-1]%2 != y%2) {
+            numbers[x-1] = y;
+            // for (int i = 0; i < int(group_index.size()); i++) {
+                // if (group_index[i].first <= x && x <= group_index[i].second) {
+                        // if (y%2==0) group[i]++; else group[i]--;
+                        // break;
+                    // }
+            // }
+            int i = lower_bound(group_index.begin(), group_index.end(), x) - group_index.begin()-1;
+            if (y%2==0) group[i]++; else group[i]--;
         }
+
+        odd = 0; 
+        for(auto e: group) {
+            if (e == 0) odd++;
+        }
+
+        if (odd%2==0) cout << "even" << endl;
+        else cout << "odd" << endl;
             
     }
     
 
     return 0;
-}
+    }
