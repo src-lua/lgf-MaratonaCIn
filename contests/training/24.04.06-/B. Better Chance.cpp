dd4cb5cb38231ef50ebf9601af7a4d23b280c129
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2024-04-06 09:42:40 -0300

    B - AC

---
+++ contests/training/24.04.06-/B. Better Chance.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cout << setprecision(15) << fixed;
+
+    int rt, rj; cin >> rt >> rj;
+    double st, sj; cin >> st >> sj;
+
+    double taoyuan = (rt-1) / st;
+    double jakarta = (rj-1) / sj;
+
+    if (abs(taoyuan - jakarta) < 10e-11) {
+        cout << "SAME";
+    }
+
+    else if (taoyuan < jakarta) {
+        cout << "TAOYUAN";
+    }
+
+    else if (jakarta < taoyuan) {
+        cout << "JAKARTA";
+    }
+
+
+
+
+    return 0;
+}
