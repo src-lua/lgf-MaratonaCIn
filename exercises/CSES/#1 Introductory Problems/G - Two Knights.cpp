ce9853b5a6ad0114b5e2ff86852d7260b58cd7a8
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2024-11-28 16:47:36 -0300

    Update G - Two Knights.cpp

--- exercises/CSES/#1 Introductory Problems/G - Two Knights.cpp
+++ exercises/CSES/#1 Introductory Problems/G - Two Knights.cpp
@@ -3,12 +3,21 @@
 #define endl '\n'
 
 using namespace std;
+using ll = long long;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    /* code */
+    int n; cin >> n;
+
+    ll answ = 0;
+
+    cout << 0 << endl;
+    
+    for (ll i = 2 ; i <= n ; i++) {
+        cout << (i * i) * (i * i - 1) / 2 - (4 * (i - 2) * (i - 1)) << endl;
+    }
 
     return 0;
 }
