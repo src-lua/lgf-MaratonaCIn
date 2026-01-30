b76ed7a5cb6d844b6221ec49a5a1e033857da445
Src-Lua <lgf@cin.ufpe.br>
2026-01-29 22:51:52 -0300

    create setup.sh to set-up bundleling experience with lgf-cplib

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-LIB_PATH="$HOME/src/lgf-cpLib/lib"
+LIB_PATH="$HOME/src/lgf-cpLib"
 VSCODE_DIR="$(dirname "$0")/.vscode"
 CPTOOLS_CONFIG="$HOME/.config/cptools/config.json"
 
@@ -25,7 +25,7 @@ cat > "$VSCODE_DIR/c_cpp_properties.json" << 'JSONEOF'
             "name": "Linux",
             "includePath": [
                 "${workspaceFolder}/**",
-                "${env:HOME}/src/lgf-cpLib/lib"
+                "${env:HOME}/src/lgf-cpLib"
             ],
             "compilerPath": "/usr/bin/g++",
             "cStandard": "c17",
@@ -50,7 +50,7 @@ cat > "$VSCODE_DIR/tasks.json" << 'JSONEOF'
                 "-O2",
                 "-std=c++17",
                 "-DLOCAL",
-                "-I${env:HOME}/src/lgf-cpLib/lib",
+                "-I${env:HOME}/src/lgf-cpLib",
                 "${file}",
                 "-o",
                 "${fileDirname}/${fileBasenameNoExtension}"
@@ -75,14 +75,14 @@ echo "VSCode config created."
 
 # --- cptools config ---
 
-INCLUDE_FLAG="-I$HOME/src/lgf-cpLib/lib"
+INCLUDE_FLAG="-I$HOME/src/lgf-cpLib"
 
 if command -v cptools &> /dev/null; then
     if [ -f "$CPTOOLS_CONFIG" ]; then
         python3 -c "
 import json, sys, os
 cfg_path = os.path.expanduser('~/.config/cptools/config.json')
-extra = ['-DLOCAL', '-I' + os.path.expanduser('~/src/lgf-cpLib/lib')]
+extra = ['-DLOCAL', '-I' + os.path.expanduser('~/src/lgf-cpLib')]
 with open(cfg_path, 'r') as f:
     cfg = json.load(f)
 flags = cfg.get('compiler_flags', ['-O2', '-std=c++17'])
