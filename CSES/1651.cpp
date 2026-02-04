2e98575dcdcee9ef38130cb0673be9d58a248d4f
Src-Lua <lgf@cin.ufpe.br>
2026-02-04 01:39:42 -0300

    CSES

---
+++ CSES/1651.cpp
@@ -0,0 +1,51 @@
+/**
+ * Author:      Lua
+ * Problem:     1651 - Range Update Queries
+ * Link:        https://cses.fi/problemset/task/1651
+ * Status:      AC
+ * Created:     04-02-2026 01:33:34
+ **/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#include "lgf-cplib/data-structures/segment-tree/dual-segment-tree.hpp"
+
+struct Tag {
+    ll add = 0;
+    void compose(const Tag& t) {
+        add += t.add;
+    }
+    void apply(ll& val) { val += add; }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int n, q; cin >> n >> q;
+    vector<ll> arr(n); for (auto &x : arr) cin >> x;
+
+    DualSegTree<ll, Tag> seg(arr);
+
+    for (int i = 0; i < q; i++) {
+        int op; cin >> op;
+
+        if (op == 1) {
+            int l, r, x; cin >> l >> r >> x; l--, r--;
+
+            seg.update(l, r, {x});
+        }
+
+        else if (op == 2) {
+            int idx; cin >> idx; idx--;
+
+            cout << seg.get(idx) << '\n';
+        }
+    }
+    
+
+    return 0;
+}
--- CSES/info.md
+++ CSES/info.md
@@ -2,10 +2,11 @@
 
 **Created**: 31-01-2026 15:13:19
 
-**Progress**: 1/1 solved
+**Progress**: 2/2 solved
 
 ## Problems
 
 | Problem | Status |
 |---------|--------|
 | [1636 - Coin Combinations II](https://cses.fi/problemset/task/1636) | ✅ AC |
+| [1651 - Range Update Queries](https://cses.fi/problemset/task/1651) | ✅ AC |
