00ce41bd4d3f7e4fbc53f16d2377e1738b05fa6e
src-lua <lua.guimaraes.src@gmail.com>
2026-05-02 13:41:38 -0300

    Trainings/teste

---
+++ Trainings/teste/F.cpp
@@ -0,0 +1,18 @@
+/**
+ * Author:      Lua
+ * Problem:     F
+ * Link:        https://codeforces.com/group/btcK4I5D5f/contest/655026/problem/F
+ * Status:      ~
+ * Created:     02-05-2026 13:40:03
+ **/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    return 0;
+}
---
+++ Trainings/teste/G.cpp
@@ -0,0 +1,18 @@
+/**
+ * Author:      Lua
+ * Problem:     G
+ * Link:        https://codeforces.com/group/btcK4I5D5f/contest/655026/problem/G
+ * Status:      ~
+ * Created:     02-05-2026 13:40:03
+ **/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    return 0;
+}
