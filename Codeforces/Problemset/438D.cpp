1ccc98618ada8a8bf91dc64be81b905f0d610131
Lua Guimarães <lua.guimaraes@incognia.com>
2026-07-17 21:20:44 -0300

    new lib import path

--- Codeforces/Problemset/438D.cpp
+++ Codeforces/Problemset/438D.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 using ll = long long;
 
-#include "lib/data-structures/segment-tree/seg-beats.hpp"
+#include "lib/data-structures/segment-tree/segment-tree-beats.hpp"
 
 const ll NO_OP = 1e18; 
 
